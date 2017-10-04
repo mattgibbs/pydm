ac3ea03d8bfb10456b58461bbce9d64331507dfa
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-10-04 10:45:58 -0700

    Set up and use PVs in demo files.

--- examples/icalepcs/demo_2.py
+++ examples/icalepcs/demo_2.py
@@ -11,14 +11,14 @@ class CorrelationFitter(Display):
         
         #add a line to the plot to represent a fit to the data.
         self.fit_line = PlotCurveItem(pen='y')
-        self.ui.waveformPlot.addItem(self.fit_line)
-        self.fit_text = TextItem()
-        self.ui.waveformPlot.addItem(self.fit_text)
+        #self.ui.waveformPlot.addItem(self.fit_line)
+        self.fit_text = TextItem(anchor=(0.5,1))
+        #self.ui.waveformPlot.addItem(self.fit_text)
         self.fit_text.setPos(5500.0, 1.0)
         #intercept redraw signal to add a fit to the data.
         self.curve = self.ui.waveformPlot._curves[0]
-        self.curve.data_changed.disconnect()
-        self.curve.data_changed.connect(self.new_curve_data)
+        #self.curve.data_changed.disconnect()
+        #self.curve.data_changed.connect(self.new_curve_data)
         
     def ui_filename(self):
         return 'demo_1.ui'
@@ -33,10 +33,13 @@ class CorrelationFitter(Display):
         fit_poly = np.poly1d(fit_coefs)
         
         #Update the data for our fit line.
-        x_vals = np.linspace(np.min(self.curve.x_waveform), np.max(self.curve.x_waveform), len(self.curve.y_waveform))
+        min_x = np.min(self.curve.x_waveform)
+        max_x = np.max(self.curve.x_waveform)
+        x_vals = np.linspace(min_x, max_x, len(self.curve.y_waveform))
         fit_data = fit_poly(x_vals)
         self.fit_line.setData(x_vals, fit_data)
-        self.fit_text.setText(str(fit_poly))
+        self.fit_text.setText("Peak at: {:.4}".format(fit_poly[1]/(2.0*fit_poly[0])))
+        self.fit_text.setPos((min_x+max_x)/2.0, min(self.curve.y_waveform))
         #Now tell the plot to redraw itself
         self.ui.waveformPlot.redrawPlot()
         
