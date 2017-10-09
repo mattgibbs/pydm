a428107b53debacafda9923df5a854bbfd670667
Fernando <fernandohds564@gmail.com>
2017-10-09 14:28:34 -0300

    Adapt PyDMLed to interact with the multiState QLed.

--- pydm/widgets/led.py
+++ pydm/widgets/led.py
@@ -16,21 +16,21 @@ class PyDMLed(QLed, PyDMWidget):
         The channel to be used by the widget.
     bit : int
         Bit of the PV value to be handled.
-    enum_map : int
-        Mapping between enum_string and led colors.
+    color_list : int
+        List of QColor objects for each state the channel can assume.
     """
 
     DarkGreen = QColor(20, 80, 10)
     LightGreen = QColor(0, 140, 0)
     Yellow = QColor(210, 205, 0)
     Red = QColor(207, 0, 0)
+    default_colorlist = [DarkGreen, LightGreen, Yellow, Red]
 
-    def __init__(self, parent=None, init_channel='', bit=-1, enum_map=None):
+    def __init__(self, parent=None, init_channel='', bit=-1, color_list=None):
         QLed.__init__(self, parent)
         PyDMWidget.__init__(self, init_channel=init_channel)
-
         self.pvbit = bit
-        self.setEnumMap(enum_map)
+        self.stateColors = color_list or self.default_colorlist
 
     @pyqtProperty(int)
     def pvbit(self):
@@ -48,16 +48,6 @@ class PyDMLed(QLed, PyDMWidget):
             self._bit = -1
             self._mask = None
 
-    # This property is not available to designer yet
-    def enumMap(self):
-        """
-        Map the PV property :attr:`enum_strings` to led colors.
-        """
-        return self._enum_map
-
-    def setEnumMap(self, enum_map):
-        self._enum_map = enum_map
-
     def value_changed(self, new_val):
         """
         Receive new value and set led color accordingly.
@@ -68,39 +58,13 @@ class PyDMLed(QLed, PyDMWidget):
         If a :attr:`bit` is set the value received will be treated as an int
         and the bit value is extracted using a mask. The led represents the
         value of the chosen bit.
-
-        If the PV is an enum type, the led has a color for each enum. If the
-        :attr:`enum_map` is set, it is used to map each enum value to a led
-        color.
         """
         PyDMWidget.value_changed(self, new_val)
         if new_val is None:
             return
-        if self.enum_strings is None:  # PV of type integer or float
-            value = int(new_val)
-            if self._bit < 0:  # Led represents value of PV
-                self.setState(1 if value else 0)
-            else:  # Led represents specific bit of PV
-                bit_val = (value & self._mask) >> self._bit
-                self.setState(bit_val)
-        else:  # PV of type ENUM
-            self.setState(1)
-            if self._enum_map is None:
-                enum_colorlist = [PyDMLed.DarkGreen,
-                                  PyDMLed.LightGreen,
-                                  PyDMLed.Yellow,
-                                  PyDMLed.Red]
-                self.setOnColor(enum_colorlist[new_val])
-            else:
-                if self.enum_strings is not None and isinstance(new_val, int):
-                    enum_name = self.enum_strings[new_val]
-                    color = self._enum_map[enum_name]
-                    self.setOnColor(color)
-
-    def enum_strings_changed(self, new_enum_strings):
-        """
-        Issue value_changed to set the led color according to the enum_strings.
-        """
-        PyDMWidget.enum_strings_changed(self, new_enum_strings)
-        if self.value is not None:
-            self.value_changed(self.value)
+        value = int(new_val)
+        if self._bit < 0:  # Led represents value of PV
+            self.setState(value)
+        else:  # Led represents specific bit of PV
+            bit_val = (value & self._mask) >> self._bit
+            self.setState(bit_val)
