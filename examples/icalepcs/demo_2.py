9e4c12c1d8489fcc72073eb78f60467209e54bb8
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-10-12 11:24:55 +0200

    Get rid of TextItem, add comment to put the peak location in a label widget.

--- examples/icalepcs/demo_2.py
+++ examples/icalepcs/demo_2.py
@@ -8,38 +8,38 @@ from pyqtgraph import PlotCurveItem, TextItem
 class CorrelationFitter(Display):
     def __init__(self, parent=None, args=None):
         super(CorrelationFitter, self).__init__(parent=parent, args=args)
-        
+    
         #add a line to the plot to represent a fit to the data.
         self.fit_line = PlotCurveItem(pen='m')
         self.ui.waveformPlot.addItem(self.fit_line)
-        self.fit_text = TextItem(anchor=(0.5,1))
-        self.ui.waveformPlot.addItem(self.fit_text)
-        self.fit_text.setPos(5500.0, 1.0)
-        #intercept redraw signal to add a fit to the data.
+        
+        #Intercept data changed signals to fit data before plotting it.
         self.curve = self.ui.waveformPlot._curves[0]
         self.curve.data_changed.disconnect()
         self.curve.data_changed.connect(self.new_curve_data)
         
     def ui_filename(self):
         return 'demo_1.ui'
-        
+
     def ui_filepath(self):
         return path.join(path.dirname(path.realpath(__file__)), self.ui_filename())
-    
+        
     @pyqtSlot()
     def new_curve_data(self):
         #Get the waveform data from the curve and fit a parabola to it.
         fit_coefs = np.polyfit(self.curve.x_waveform, self.curve.y_waveform, 2)
         fit_poly = np.poly1d(fit_coefs)
-        
+
         #Update the data for our fit line.
         min_x = np.min(self.curve.x_waveform)
         max_x = np.max(self.curve.x_waveform)
         x_vals = np.linspace(min_x, max_x, len(self.curve.y_waveform))
         fit_data = fit_poly(x_vals)
         self.fit_line.setData(x_vals, fit_data)
-        self.fit_text.setText("Peak at: {:.4}".format((-1.0*fit_poly[1])/(2.0*fit_poly[2])))
-        self.fit_text.setPos((min_x+max_x)/2.0, min(self.curve.y_waveform))
+        
+        #Calculate the position of the peak, put it in a label.
+        #self.ui.peakLabel.setText("Peak at: {:.4}".format((-1.0*fit_poly[1])/(2.0*fit_poly[2])))
+        
         #Now tell the plot to redraw itself
         self.ui.waveformPlot.redrawPlot()
         
