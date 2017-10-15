328c64126e37a7725dfcabaa63dadf1c4a617d74
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-10-15 12:24:52 -0700

    Add some PVs for a non-time-scrolling version of the demo - works around a problem where X and Y waveforms sometimes get out to sync.

--- examples/icalepcs/demo_2.py
+++ examples/icalepcs/demo_2.py
@@ -8,14 +8,14 @@ class CorrelationFitter(Display):
     def __init__(self, parent=None, args=None):
         super(CorrelationFitter, self).__init__(parent=parent, args=args)
     
-        # #add a line to the plot to represent a fit to the data.
-        # self.fit_line = PlotCurveItem(pen='w')
-        # self.ui.waveformPlot.addItem(self.fit_line)
-        
-        # #Intercept data changed signals to fit data before plotting it.
-        # self.curve = self.ui.waveformPlot._curves[0]
-        # self.curve.data_changed.disconnect()
-        # self.curve.data_changed.connect(self.new_curve_data)
+        #add a line to the plot to represent a fit to the data.
+        self.fit_line = PlotCurveItem(pen='w')
+        self.ui.waveformPlot.addItem(self.fit_line)
+
+        #Intercept data changed signals to fit data before plotting it.
+        self.curve = self.ui.waveformPlot._curves[0]
+        self.curve.data_changed.disconnect()
+        self.curve.data_changed.connect(self.new_curve_data)
         
     def ui_filename(self):
         return 'demo_1.ui'
@@ -23,24 +23,24 @@ class CorrelationFitter(Display):
     def ui_filepath(self):
         return path.join(path.dirname(path.realpath(__file__)), self.ui_filename())
         
-    # @pyqtSlot()
-    # def new_curve_data(self):
-        # #Get the waveform data from the curve and fit a parabola to it.
-        # fit_coefs = np.polyfit(self.curve.x_waveform, self.curve.y_waveform, 2)
-        # fit_poly = np.poly1d(fit_coefs)
+    @pyqtSlot()
+    def new_curve_data(self):
+        #Get the waveform data from the curve and fit a parabola to it.
+        fit_coefs = np.polyfit(self.curve.x_waveform, self.curve.y_waveform, 2)
+        fit_poly = np.poly1d(fit_coefs)
 
-        # #Update the data for our fit line.
-        # min_x = np.min(self.curve.x_waveform)
-        # max_x = np.max(self.curve.x_waveform)
-        # x_vals = np.linspace(min_x, max_x, len(self.curve.y_waveform))
-        # fit_data = fit_poly(x_vals)
-        # self.fit_line.setData(x_vals, fit_data)
-        
-        # #Now tell the plot to redraw itself
-        # self.ui.waveformPlot.redrawPlot()
+        #Update the data for our fit line.
+        min_x = np.min(self.curve.x_waveform)
+        max_x = np.max(self.curve.x_waveform)
+        x_vals = np.linspace(0.8*min_x, 1.2*max_x, len(self.curve.y_waveform))
+        fit_data = fit_poly(x_vals)
+        self.fit_line.setData(x_vals, fit_data)
+
+        #Now tell the plot to redraw itself
+        self.ui.waveformPlot.redrawPlot()
         
         # #Calculate the position of the peak, put it in a label.
-        # self.ui.peakLabel.setText("Peak at: {:.4}".format((-1.0*fit_poly[1])/(2.0*fit_poly[2])))
+        self.ui.peakLabel.setText("Peak at: {:.4}".format((-1.0*fit_poly[1])/(2.0*fit_poly[2])))
         
         
         
